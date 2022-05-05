565ebe430fb311b978681b3e1658b1bb9b377976
Adan <adanaligakhar@gmail.com>
2022-05-06 01:28:49 +0500

    worked on charts and created other components

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,26 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const {githubUser} = React.useContext(GithubContext);
+  const {avatar_url, html_url,name,company,blog,bio,location,twitter_username} = githubUser;
+  return <Wrapper>
+    <header>
+      <img src={avatar_url} alt={name}/>
+      <div>
+        <h4>{name}</h4>
+        <p>@{twitter_username ?? 'not given'}</p>
+      </div>
+      <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio ?? 'bio not given'}</p>
+      <div className='links'>
+        <p><MdBusiness/> {company ?? 'company not provided' }</p>
+        <p><MdLocationOn/> {location}</p>
+        <a  href={blog ? `https://${blog}` : '#'}><MdLink></MdLink>{blog || 'no blog exists'}</a>
+        
+      </div>
+    
+  </Wrapper>
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,7 +3,46 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+
+  const {repos} = React.useContext(GithubContext);
+
+  let languages = repos.reduce((total,item) =>{
+    const {language} = item;
+    if(!language) return total;
+
+    if(!total[language]){
+      total[language] = {label : language, value : 1}
+    }else{
+      total[language] = {...total[language],value : total[language].value + 1}
+    }
+    return total;
+  },{})
+
+  console.log(languages)
+ languages = Object.values(languages).sort((a,b) => {
+   return b.value - a.value;
+ }).slice(0,5)
+ 
+
+
+
+
+  const chartData = [
+    ...languages
+  ];
+
+  return <section>
+    <Wrapper className='section-center'>
+       {/* <ExampleChart data={chartData}/> */}
+       <Pie3D data={chartData}/>
+       <div></div>
+         <Doughnut2D data={chartData}/>
+       <div></div>
+    </Wrapper>
+
+  </section>
+  
+  
 };
 
 const Wrapper = styled.div`
